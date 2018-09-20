93a204c4119efcc99f66bad4431d033cb06497ab
Art Simon <simart@gmail.com>
2018-09-20 11:17:14 -0700

    Create p5test2.js

---
+++ p5test2.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(200, 100);
+}
+
+function draw() {
+  fill(255, 255, 0);
+  ellipse(100, 50, 80, 80);
+  arc(100,50,60,60,PI/8,7*PI/8);
+  fill(0, 0, 0);
+  ellipse(85, 40, 10, 15);
+  ellipse(115, 40, 10, 15);
+}
